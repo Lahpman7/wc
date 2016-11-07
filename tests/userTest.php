b20b17ffcaebb9d3b6801af8af962863438aa8b6
Lahpman7 <maslopez@csumb.edu>
2016-11-07 21:05:58 +0000

    Fixed Facebook api issues, updated to recent version. Added unit tests update email, update grapes for winebottles

--- tests/userTest.php
+++ tests/userTest.php
@@ -4,17 +4,30 @@ include_once("../lib/user.php");
 //include "lib/user.php";
 class UserTest extends TestCase
 {
+    
     public function testUser(){
         $newUser = new RegUser();
         $tester = $newUser::isUser("Testy2");
         //assertEquals(-1, $b->getAmount());
-        $this->assertEquals($tester,false);
+        $this->assertEquals($tester,true);
     }
     
     public function testDeleteUser(){
         $newUser = new RegUser();
         $tester = $newUser::deleteUser("asdf");
         $this->assertTrue($tester);
+    }
+    
+    public function testUpdateEmail(){
+        $updateUser = new RegUser();
+        $tester = $updateUser::updateEmail('remix@yahoo.organization','Testy2');
+        $this->assertEquals($tester,true);
+    }
+    
+    public function testUpdateZip(){
+        $updateUser = new RegUser();
+        $tester = $updateUser::updateZip(72689,"Testy2");
+        $this->assertEquals($tester,true);
     }
 }
 ?>
