5bca9e217bc3963f41b52be436ae0611e7b80496
Mason <maslopez@csumb.edu>
2016-11-10 05:46:58 -0800

    error reporting update

--- api/login.php
+++ api/login.php
@@ -3,9 +3,9 @@
         echo "above session\n";
         session_start();
         echo "above require\n";
-        //include '../vendor/autoload.php';
+        include '../vendor/autoload.php';
         //include 'db.include.php';
-        require_once __DIR__ . '/vendor/autoload.php';
+        //require_once __DIR__ . '/vendor/autoload.php';
         //above works on my test page but maybe because I have all in root dir
         echo "under require\n";
         $fb = new Facebook\Facebook([
