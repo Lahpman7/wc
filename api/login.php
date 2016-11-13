a0f8f3a3711ad2a9101df3eb4d1a88b23b5e2db7
Mason <maslopez@csumb.edu>
2016-11-12 23:31:09 -0800

    bug fixed

--- api/login.php
+++ api/login.php
@@ -64,39 +64,28 @@
         		exit;
         	}
         	$image = 'https://graph.facebook.com/'.$payload['id'].'/picture?width=200';
-	    	//prints image
-	    	//echo "<img src='$image' /><br><br>";
+  	    	$_SESSION['username'] = $payload['email'];
+  	    	$_SESSION['fullname'] = $payload['name'];
+          $_SESSION['first'] = $payload['first_name'];
+          $_SESSION['last'] = $payload['last_name'];
+  	    	$_SESSION['imageUrl'] = $image;
+          $_SESSION['age'] = $payload['age_range']['min'];
 
-	    	$_SESSION['username'] = $payload['email'];
-	    	$_SESSION['fullname'] = $payload['name'];
-        $_SESSION['first'] = $payload['first_name'];
-        $_SESSION['last'] = $payload['last_name'];
-	    	$_SESSION['imageUrl'] = $image;
-        $_SESSION['age'] = $payload['age_range']['min'];
-        //var_dump($payload);
-        //exit;
-	    	//can invoke insert function or function that sees if its in DB then inserts if not
-        $testUser = new RegUser();
-        if($testUser::emailExists($_SESSION['username'])){
-          echo 'in existence';
-          //when user email is already in our db, we send them back to index
-          //header('Location: http://localhost/wc/#!/user-profile');
-          //header('Location: http://wcdeploy.csztpytway.us-west-1.elasticbeanstalk.com/#!/user-profile')
-        }
-        else{
-          echo 'not in existance';
-          //we are gonna send user to finish sign up in a HIDDEN form.
-          //header('Location: http://localhost/wc/#!/register-account-fb');
-          //header('Location: http://wcdeploy.csztpytway.us-west-1.elasticbeanstalk.com/#!/register-account-fb')
-        }
-	    	//header('Location: ../index.php');
-	    	//var_dump($payload);
-        	//echo $payload['age_range']['min'];
-          	// Now you can redirect to another page and use the access token from $_SESSION['accessToken']
+  	    	// function that checks if its in DB then inserts if not
+          $testUser = new RegUser();
+          if($testUser::emailExists($_SESSION['username'])){
+            //when user email is already in our db, we send them back to index
+            //header('Location: http://localhost/wc/#!/user-profile');
+            header('Location: http://wcdeploy.csztpytway.us-west-1.elasticbeanstalk.com/#!/user-profile')
+          }
+          else{
+            //we are gonna send user to finish sign up in a HIDDEN form.
+            //header('Location: http://localhost/wc/#!/register-account-fb');
+            header('Location: http://wcdeploy.csztpytway.us-west-1.elasticbeanstalk.com/#!/register-account-fb')
+          }
+
         } else {
-        	// will need to change this to AWS, cannot test locally!!! facebook requires http/s
         	$loginUrl = $helper->getLoginUrl($urlReturn, $permissions);
-          //echo 'in redirect to facebook';
         	header('Location: ' . $loginUrl);
         }
     ?>
