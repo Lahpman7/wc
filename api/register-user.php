e7c798833e7517b28e646ad3d468af3749871879
Mason <maslopez@csumb.edu>
2016-12-05 13:56:46 -0800

    adding logging features for when users are created and for when bottles are registered
    Summary: Added logging features for when users are created and when bottles are registered
    
    Test Plan:
    1. Review the code
    2. Check to see if things logging
    3. Checkout date format
    
    Reviewers: AnitaGarcia819
    
    Reviewed By: AnitaGarcia819
    
    Differential Revision: http://csumbse.net/D654

--- api/register-user.php
+++ api/register-user.php
@@ -6,7 +6,8 @@ require_once("../lib/user.php");
 if(isset($_POST["regAccount"])){
 
     date_default_timezone_set('America/Los_Angeles');
-    $date = date('l jS \of F Y h:i:s A');
+    $dateStamp = date('l jS \of F Y h:i:s A');
+
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
@@ -19,12 +20,18 @@ if(isset($_POST["regAccount"])){
     $date = $_POST['date'];
     $imgUrl = "images/default_profile_img.png";
     $user = new RegUser();
-    $log = $date . ' :' .  $username. ' ' .$email. ' ' .$firstname. ' ' .$lastname. ' ' . $age . ' ' .
-              $zipcode. ' ' .$employment. ' ' .$cert. ' ' .$date. ' ' .$imgUrl .' \n';
+
+    $pw2 = sha1($password);
+    $log = $dateStamp . '-> Account Created, Details: user: ' .  $username . ' pass:'. $pw2 .' email: ' .$email. ' first: ' .$firstname. ' last: ' .$lastname. ' age: ' . $age . ' zip ' .
+              $zipcode . ' employment: ' . $employment . ' certification:  ' . $cert . ' date_cert: ' . $date . ' imgLoc: ' . $imgUrl .' \n';
     error_log($log);
+
     $user::insertUser($username,$password,$email,$firstname,$lastname,$age,$zipcode,$employment,$cert,$date, $imgUrl);
 }
 if(isset($_POST['regAccountFB'])){
+    date_default_timezone_set('America/Los_Angeles');
+    $dateStamp = date('l jS \of F Y h:i:s A');
+
     $username = $_POST['username'];
     $password = $_POST['password'];
     $zipcode = $_POST['zipcode'];
@@ -39,6 +46,12 @@ if(isset($_POST['regAccountFB'])){
     $imgUrl = $_SESSION['imageUrl'];
     echo $username. ' ' . $password.' '. $email.' '. $firstname.' '. $lastname.' ' .$age.' '.$zipcode.' '.$employment.' '. $cert_body.' '.$date;
     $user = new RegUser();
+
+    $pw2 = sha1($password);
+    $log = $dateStamp . '-> Account Created, Details: user: ' .  $username . ' pass:'. $pw2 .' email: ' .$email. ' first: ' .$firstname. ' last: ' .$lastname. ' age: ' . $age . ' zip ' .
+              $zipcode . ' employment: ' . $employment . ' certification:  ' . $cert_body . ' date_cert: ' . $date . ' imgLoc: ' . $imgUrl .' \n';
+    error_log($log);
+
     $user::insertUser($username,$password,$email,$firstname,$lastname,$age,$zipcode,$employment,$cert_body,$date, $imgUrl);
     //need username, pass, age, zip, employment, cert body and date
 }
--- api/register-wine.php
+++ api/register-wine.php
@@ -1,10 +1,20 @@
 <?php
 
     require_once("../lib/wineBottle.php");
+    date_default_timezone_set('America/Los_Angeles');
+    $date = date('l jS \of F Y h:i:s A');
+
     if(isset($_POST['regWine'])){
         $regBottle = new RegBottle();
         $regBottle::insertBottle($_POST["producer"], $_POST["wname"], $_POST["vintage"], $_POST['wine_styles'],
         $_POST["grape"], $_POST["country"], $_POST["state"], $_POST["region"], $_POST["alcohol"]);
+
+        $log = $date . ' Success, Bottle Registered ---> Producer: ' $_POST["producer"].' Wine Name: '. $_POST["wname"].' Vintage: ' . $_POST["vintage"].' Wine Style: ' . $_POST['wine_styles'].' Grape: ' .
+        $_POST["grape"].' Country: ' . $_POST["country"].' State: ' . $_POST["state"].' Region: ' . $_POST["region"].' Alcohol \% ' . $_POST["alcohol"] .'\n';
     }
+    else{
+        $log = $date . 'Unauthorized access to register-wine.php, post not set, redirect home.';
+    }
+    error_log($log);
     header("Location: ../index.php");
 ?>
